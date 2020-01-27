9f34952635384425a9ff5327008b7c4b3a1a6555
Benedikt Bergenthal <benedikt.bergenthal@imflow.me>
2020-01-27 12:36:51 +0100

    pylint conforming commit
    Signed-off-by: Benedikt Bergenthal <benedikt@kdrennert.de>

--- configs/gunicorn/gunicorn.conf.py
+++ configs/gunicorn/gunicorn.conf.py
@@ -1,24 +1,26 @@
+"""
+Configuration for gunicorn when running in docker container
+"""
 # coding=utf-8
 # Reference: https://github.com/benoitc/gunicorn/blob/master/examples/example_config.py
 import os
 import multiprocessing
 
+# pylint: disable=invalid-name
 _ROOT = "/app"
 _VAR = os.path.join(_ROOT, 'var')
 _ETC = os.path.join(_ROOT, 'etc')
 
 loglevel = 'info'
-# errorlog = os.path.join(_VAR, 'log/api-error.log')
-# accesslog = os.path.join(_VAR, 'log/api-access.log')
 errorlog = "-"
 accesslog = "-"
 
-# bind = 'unix:%s' % os.path.join(_VAR, 'run/gunicorn.sock')
 bind = '0.0.0.0:5000'
-# workers = 3
+
 workers = multiprocessing.cpu_count() * 2 + 1
 
 timeout = 3 * 60  # 3 minutes
 keepalive = 24 * 60 * 60  # 1 day
 
 capture_output = True
+# pylint: enable=invalid-name
