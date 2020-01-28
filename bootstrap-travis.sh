9930b8b1265ef7bd33a208aa14a480ddf146f70c
Benedikt Bergenthal <benedikt.bergenthal@imflow.me>
2020-01-28 13:57:43 +0100

    fixed typo
    Signed-off-by: Benedikt Bergenthal <benedikt@kdrennert.de>

--- bootstrap-travis.sh
+++ bootstrap-travis.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt install -y --no-install-recommends gunicorn xvfb wget libnss3 libxss1 libappindicator1 libindicator7 libsdl1.2-dev fonts-liberation libappindicator3-1 libasound2 libatk-bridge2.0-0 libgtk-3-0 libnspr4 libnss3 libxtst6 lsb-release xdg-utils enchant
-cs /usr/bin
+cd /usr/bin
 sudo wget https://chromedriver.storage.googleapis.com/79.0.3945.36/chromedriver_linux64.zip
 sudo unzip chromedriver_linux64.zip
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb \
