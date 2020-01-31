1e7a057078c1ab67d0712a84ad99fa7de946f8a9
Benedikt Bergenthal <benedikt.bergenthal@imflow.me>
2020-01-31 10:27:14 +0100

    added python path to wsgi.py
    Signed-off-by: Benedikt Bergenthal <benedikt.bergenthal@imflow.me>

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,10 @@
 """
 WSGI config for the running of the app with gunicorn
 """
+import sys
+import os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/url2_image/')
 from url2_image.app import app
 
 if __name__ == '__main__':
